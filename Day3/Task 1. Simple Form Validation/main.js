d9eb3b7a12bd26b75a959cf7fd20fd3601b671ab
nardany <99093283+nardany@users.noreply.github.com>
2024-02-21 21:54:47 +0400

    Add files via upload

--- Day3/Task 1. Simple Form Validation/main.js
+++ Day3/Task 1. Simple Form Validation/main.js
@@ -1,1 +1,6 @@
+let btn = document.getElementById("btn")
+let inp = document.getElementById("inp")
 
+const checkEmail = function(){
+    inp.value === "" ? alert("Please enter an email address") : alert("successful login")
+}
