0af5930651a44bb4539729d4ccbf186010dfd890
nardany <99093283+nardany@users.noreply.github.com>
2024-02-21 21:58:27 +0400

    Add files via upload

--- Day3/Task 3. Basic HTML Form with Validation/main.js
+++ Day3/Task 3. Basic HTML Form with Validation/main.js
@@ -1,1 +1,14 @@
+let nameInput = document.getElementById("nameInput")
+let emailInput = document.getElementById("emailInput")
+let btn= document.getElementById("btn")
 
+
+btn.addEventListener("click", check)
+function check(){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(emailRegex.test(emailInput.value) && emailInput.value !== "" && nameInput.value !== ""){
+            alert("This is corect")
+    }else{
+        alert("this is isCorect")
+    }
+}
