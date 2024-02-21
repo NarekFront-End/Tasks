27923da2e7d4554e81551282b61a0b6090814fd1
nardany <99093283+nardany@users.noreply.github.com>
2024-02-21 22:00:15 +0400

    Add files via upload

--- Day3/Task 4. Implement a Basic Slider with Array Data/main.js
+++ Day3/Task 4. Implement a Basic Slider with Array Data/main.js
@@ -1,1 +1,18 @@
+let imageSources = [
+    'walp1.jpg',
+    'walp2.jpg',
+    'walp3.jpg'
+]
 
+let currentIndex = 0;
+
+document.getElementById('walp1').src = imageSources[currentIndex];
+
+const nextSlide = function(){
+    currentIndex = (currentIndex + 1) % imageSources.length
+    document.getElementById("walp1").src = imageSources[currentIndex]
+}   
+const previousSlide = function(){
+    currentIndex = (currentIndex - 1 + imageSources.length) % imageSources.length
+    document.getElementById("walp1").src = imageSources[currentIndex];
+}
