e056ea7e03d0d1833860a48d96fba6fe2b980660
nardany <99093283+nardany@users.noreply.github.com>
2024-02-21 21:49:04 +0400

    Add files via upload

--- Day2/Task 2. Display User Input/main.js
+++ Day2/Task 2. Display User Input/main.js
@@ -1,1 +1,9 @@
+let input = document.getElementById("input")
+let btn = document.getElementById("btn")
+let p = document.getElementById("p")
 
+
+const clickDispley = function(){
+    p.innerHTML = input.value 
+    input.value = ""
+}
