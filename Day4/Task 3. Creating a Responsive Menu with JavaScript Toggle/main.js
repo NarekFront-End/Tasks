592d89869ae42644e873b4fdfddab164c8a35c00
nardany <99093283+nardany@users.noreply.github.com>
2024-02-22 14:17:05 +0400

    Add files via upload

--- Day4/Task 3. Creating a Responsive Menu with JavaScript Toggle/main.js
+++ Day4/Task 3. Creating a Responsive Menu with JavaScript Toggle/main.js
@@ -1,1 +1,5 @@
-
+let img = document.getElementById("img")
+let navBar = document.getElementById("hNavBar")
+const hamburger = function(){
+   navBar.style.display = navBar.style.display === 'none' ? 'flex' : 'none'
+}
