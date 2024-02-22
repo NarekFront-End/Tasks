b29a9ad63aba4c5f78dd8001a9e3f2cce22195a1
nardany <99093283+nardany@users.noreply.github.com>
2024-02-22 18:48:48 +0400

    Add files via upload

--- Day4/Task 2. Form Auto-Save to LocalStorage/main.js
+++ Day4/Task 2. Form Auto-Save to LocalStorage/main.js
@@ -1,1 +1,11 @@
+let inp1 = document.getElementById("inp1")
+let inp2 = document.getElementById("inp2")
+let btn = document.getElementById("btn")
 
+
+const localStorege = function(){
+    localStorage.setItem('name',inp1.value)
+    localStorage.setItem('email',inp2.value)
+    inp1.value = ''
+    inp2.value = '' 
+}
